c316bb5124bb9840e2ae8ff08774ed7b750782c5
Naveen <Naveen@DESKTOP-HSB22VK>
2022-11-03 02:12:35 +0530

    Add project files.

---
+++ azurewebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using azurewebApp.Models;
+using azurewebApp.Services;
+
+namespace azurewebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products; 
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products= productService.GetProducts();
+
+        }
+    }
+}
