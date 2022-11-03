bbb26d057c8c15f9d03ddb5d81ee5c56d841842f
naveen naveen <nav14sept@gmail.com>
2022-11-04 04:32:32 +0530

    app config changes

--- azurewebApp/Program.cs
+++ azurewebApp/Program.cs
@@ -2,11 +2,21 @@ using azurewebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = "Endpoint=https://az204app-config.azconfig.io;Id=S9jz-l4-s0:fY1sLbnhGN6R6IrD0nIN;Secret=0PsOFUsI0Zz7m0NMGqiVVFPOBFrQ7PirFj2gu8GAMkU=";
+
+builder.Host.ConfigureAppConfiguration(builder =>
+{
+    builder.AddAzureAppConfiguration(connectionString);
+}
+);
+
+
 builder.Services.AddTransient<IProductService, ProductService>();
 
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
